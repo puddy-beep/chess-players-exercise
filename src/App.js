4f59ee6b5de780d55355b8a58fe36d07593ec9fc
Stephanie Partel <partel.stephanie@gmail.com>
2020-12-12 02:31:10 -0300

    first part of the application

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function Encapsulamento() {
+  return (
+    <div> 
+      a 
+    </div>
+  ); // everything you return in a function must be encapsulated inside a tag (this case, an empty div)
+}
+
+export default Encapsulamento;
